a9b63476f3efc46691f3f0d24dd3019ef075961d
Setu Potnis <46113818+setupotnis@users.noreply.github.com>
2019-07-08 15:20:14 -0400

    added animation for circle and timer

--- javascript-meditation-app/app.js
+++ javascript-meditation-app/app.js
@@ -2,7 +2,7 @@ const app = () => {
     const song = document.querySelector('.song');
     const play = document.querySelector('.play');
     const outline = document.querySelector('.moving-outline circle');
-    const video = document.querySelector('vid-container video');
+    const video = document.querySelector('.vid-container video');
 
     //Sounds
     const sounds = document.querySelectorAll('.sound-picker button');
@@ -13,7 +13,37 @@ const app = () => {
     //Duration
     let fakeDuration = 600;
         
-        outline.style.strokeDasharray = outlineLength;
-        outline.style.strokeDashoffset = outlineLength;
+    outline.style.strokeDasharray = outlineLength;
+    outline.style.strokeDashoffset = outlineLength;
+
+        //Play sound
+        play.addEventListener('click', () => {
+            checkPlaying(song);
+        });
+        //function for stopping and playing the sound
+        const checkPlaying = song => {
+            if(song.paused){
+                song.play();
+                video.play();
+                play.src = './svg/pause.svg';
+            } else{
+                song.pause();
+                video.pause();
+                play.src = './svg/play.svg';
+            }
+        }; 
+    //animating the circle
+    song.ontimeupdate = () => {
+        let currentTime = song.currentTime;
+        let elapsed = fakeDuration - currentTime;
+        let seconds = Math.floor(elapsed % 60);
+        let minutes = Math.floor(elapsed / 60);
+
+        let progress = outlineLength - (currentTime / fakeDuration) * outlineLength;
+        outline.style.strokeDashoffset = progress;
+        //Animate the text 
+        timeDisplay.textContent = `${minutes}:${seconds}`
+    };
+    
 };
 app();
